75527a8231189298ad5eed1c80149b6fea3a6583
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-14 15:14:05 -0700

    create skeletons for user and session controllers; update routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,44 @@
 #
 
 class User < ApplicationRecord
+
+  # add associations
+
+  validates :username, :password_digest, :session_token, :email, :name, presence: true
+  validates :username, :session_token, :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token
+    self.session_token = generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resources :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
   root "static_pages#root"
 end
