2729ca55f07022a94ee78fb14080395711d1249a
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-15 11:02:51 -0700

    build session form container and build session form component skeleton

---
+++ frontend/components/session_form/session_form_container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { signup, login, logout } from '../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = (state, ownProps) => ({
+  loggedIn: Boolean(state.session.currentUser),
+  errors: state.session.errors,
+  formType: ownProps.location.pathname.slice(1) === 'login' ? 'login' : 'signup'
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  let processForm = ownProps.location.pathname.slice(1) === 'login' ? 'login' : 'signup';
+
+  return {
+    processForm: user => dispatch(processForm(user))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SessionForm);
