1d4a06d65c18a9224416ccdd4ba31774e2897fd8
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-16 15:13:31 -0700

    Finish photo reducer.

---
+++ frontend/reducers/photo_reducer.jsx
@@ -0,0 +1,20 @@
+import { RECEIVE_PHOTOS,
+  RECEIVE_PHOTO,
+  REMOVE_PHOTO } from '../actions/photo_actions';
+import merge from 'lodash/merge';
+
+const photoReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_PHOTOS:
+      return merge({}, action.photos);
+    case RECEIVE_PHOTO:
+      return merge({}, state, {[action.photo.id]: action.photo});
+    case REMOVE_PHOTO:
+      let newState = merge({}, state);
+      delete newState[action.photoId];
+      return newState;
+    default:
+      return state;
+  }
+};
