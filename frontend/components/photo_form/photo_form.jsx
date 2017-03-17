e9369f14a3307ee26e7a8c7a67893e17f6e00fb8
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-16 17:42:50 -0700

    Add uploader to photo form component. Begin URL extraction and DB work.

--- frontend/components/photo_form/photo_form.jsx
+++ frontend/components/photo_form/photo_form.jsx
@@ -3,16 +3,65 @@ import { Link, withRouter } from 'react-router';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 
+const CLOUDINARY_UPLOAD_PRESET = 'igpreset';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/instagramme/image/upload';
+
 class PhotoForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      uploadedFileCloudinaryUrl: ''
+    };
+  }
 
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                        .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                        .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
   }
 
   render () {
     return (
       <div>
-        Hello!
+        <Dropzone
+          multiple={false}
+          accept="image/*"
+          onDrop={this.onImageDrop.bind(this)}>
+          <p>Drop an image or click to select a file to upload.</p>
+        </Dropzone>
+        <div>
+          <div className="FileUpload">
+            ...
+          </div>
+
+          <div>
+            {this.state.uploadedFileCloudinaryUrl === '' ? null :
+            <div>
+              <p>{this.state.uploadedFile.name}</p>
+              <img src={this.state.uploadedFileCloudinaryUrl} />
+            </div>}
+          </div>
+        </div>
       </div>
     );
   }
--- frontend/components/photo_form/photo_form_container.js
+++ frontend/components/photo_form/photo_form_container.js
@@ -7,7 +7,9 @@ const mapStateToProps = (state, ownProps) => ({
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
 
+  };
 };
 
 export default connect(
