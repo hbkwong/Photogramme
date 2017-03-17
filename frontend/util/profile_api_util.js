d33e8083ccb236919d496be398f9fb711637ff48
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-17 14:12:47 -0700

    Finish profile api util. Begin profile actions.

---
+++ frontend/util/profile_api_util.js
@@ -0,0 +1,24 @@
+export const fetchProfile = id => (
+  $.ajax ({
+    method: 'GET',
+    url: `/api/users/${id}`
+  })
+);
+
+export const updateUserPhoto = user => (
+  $.ajax ({
+    method: 'PATCH',
+    url: `/api/users/${user.get("user[id]")}`,
+    contentType: false,
+    processData: false,
+    data: user
+  })
+);
+
+export const updateUser = user => {
+  return $.ajax ({
+    method: "PATCH",
+    url: `/api/users/${user.id}`,
+    data: { user }
+  });
+};
