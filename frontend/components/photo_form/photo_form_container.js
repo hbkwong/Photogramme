6c3cbce5e8921afe690c06d3b5db68927add3993
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-18 22:10:05 -0700

    Update photo form container. Begin profile presentational component.

--- frontend/components/photo_form/photo_form_container.js
+++ frontend/components/photo_form/photo_form_container.js
@@ -2,11 +2,11 @@ import { connect } from 'react-redux';
 import { requestPhotos, requestPhoto, addPhoto, deletePhoto } from '../../actions/photo_actions';
 import PhotoForm from './photo_form';
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addPhoto: photo => dispatch(addPhoto(photo)),
-  };
-};
+const mapDispatchToProps = (dispatch) => (
+  {
+    addPhoto: photo => dispatch(addPhoto(photo))
+  }
+);
 
 export default connect(
   null,
---
+++ frontend/components/profile/profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+}
