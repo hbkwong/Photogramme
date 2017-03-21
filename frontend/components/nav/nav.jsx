25d469c928258851ccfc7b2f66070998dc3055bb
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-21 10:05:56 -0700

    Continue styling nav bar. Add links to nav icons.

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -6,16 +6,17 @@ const Nav = (props) => {
     <div className="nav-section">
 
       <div className="nav-logo">
-        <Link to="/">
+        <Link className="nav-link" to="/">
           <img src="http://res.cloudinary.com/instagramme/image/upload/v1490041469/Instagram_Logo_rgofax.png"/>
+          <div className="nav-divider"></div>
+          <div className="nav-instagramme">Instagramme</div>
         </Link>
-        <div className="nav-instagramme">
-          Instagramme
-        </div>
       </div>
 
       <div className="nav-icons">
-        (some icons here)
+        <Link className="nav-profile-link" to={`/${props.currentUser.id}`}>
+          <i className="fa fa-user-circle-o" aria-hidden="true"></i>
+        </Link>
       </div>
     </div>
   );
