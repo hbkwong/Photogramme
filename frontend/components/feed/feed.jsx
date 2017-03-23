6c66d644c56c30532968ecf3852a503829d00fd9
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-22 22:19:27 -0700

    Continue styling feed.

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -30,30 +30,51 @@ class Feed extends React.Component {
 
   render () {
     return (
-      <div className="feed-photos">
+      <section className="feed-photos">
         { this.props.photos.map((photo, idx) => (
-          <li key={idx}>
+          <li className="feed-post" key={idx}>
 
-            <div className="feed-profile-photo">
-              <img src={photo.user.profile_photo_url}></img>
+            <div className="feed-photo-header">
+              <div className="feed-user-info">
+                <img src={photo.user.profile_photo_url} />
+
+                <div className="username-and-location">
+                  <div className="feed-username">{photo.user.username}</div>
+                  <div className="feed-location">{photo.location}</div>
+                </div>
+
+              </div>
+              <div className="number">{photo.time}</div>
             </div>
-            <div className="feed-username">{photo.user.username}</div>
-            <div className="feed-location">{photo.location}</div>
-            <div className="feed-time">{photo.time}</div>
 
-            <img className="feed-photo" src={photo.url} />
+            <div className="feed-photo">
+              <img src={photo.url} />
+            </div>
 
-            <div className="feed-likes">{photo.likes.length} Likes</div>
+            <div className="feed-photo-info">
+              <div className="feed-likes">
+                {photo.likes.length} Likes
+              </div>
 
-            { photo.comments.map((comment, i) => (
-              <li className="feed-comments" key={i}>
-                <b>{comment.user.username}</b> {comment.body}
-              </li>
-            ))}
+              <div className="feed-photo-comments">
+                { photo.comments.map((comment, i) => (
+                  <li className="feed-comments" key={i}>
+                    <b>{comment.user.username}</b> {comment.body}
+                  </li>
+                ))}
+              </div>
+            </div>
+
+            <div className="feed-photo-footer">
+              <input
+                className="comment-input"
+                type="text"
+                placeholder="Add a comment..." />
+            </div>
 
           </li>))
         }
-      </div>
+      </section>
     );
   }
 }
