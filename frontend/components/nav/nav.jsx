b33b66177fc81342b39a86197b7138bb130b8c67
Andrew Wong <hbkwong@ucdavis.edu>
2017-03-24 09:57:53 -0700

    Add logout icon to nav.

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,34 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
 
-const Nav = (props) => {
-  if (props.currentUser) {
-    return (
-      <div className="nav-section">
+class Nav extends React.Component {
 
-        <div className="nav-logo">
-          <Link className="nav-link" to="/">
-            <img src="http://res.cloudinary.com/instagramme/image/upload/v1490041469/Instagram_Logo_rgofax.png"/>
-            <div className="nav-divider"></div>
-            <div className="nav-instagramme">Instagramme</div>
-          </Link>
-        </div>
-
-        <div className="nav-icons">
-          <Link className="nav-upload-link" to="/upload">
-            <i className="fa fa-camera fa-2x" aria-hidden="true"></i>
-          </Link>
-          <Link className="nav-profile-link" to={`/${props.currentUser.id}`}>
-            <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
-          </Link>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div> </div>
-    );
+  constructor (props) {
+    super(props);
+    this.logoutAndRedirect = this.logoutAndRedirect.bind(this);
   }
-};
+
+  logoutAndRedirect () {
+    this.props.logout()
+    .then(() => hashHistory.push('/'))
+    .then(() => hashHistory.push('/login'));
+  }
+
+  render () {
+    if (this.props.currentUser) {
+      return (
+        <div className="nav-section">
+
+          <div className="nav-logo">
+            <Link className="nav-link" to="/">
+              <img src="http://res.cloudinary.com/instagramme/image/upload/v1490041469/Instagram_Logo_rgofax.png"/>
+              <div className="nav-divider"></div>
+              <div className="nav-instagramme">Instagramme</div>
+            </Link>
+          </div>
+
+          <div className="nav-icons">
+            <Link className="nav-upload-link" to="/upload">
+              <i className="fa fa-camera fa-2x" aria-hidden="true"></i>
+            </Link>
+            <Link className="nav-profile-link" to={`/${this.props.currentUser.id}`}>
+              <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+            </Link>
+            <button className="nav-logout-link" onClick={this.logoutAndRedirect}>
+              <i className="fa fa-sign-out fa-2x" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div> </div>
+      );
+    }
+  }
+
+}
 
 export default Nav;
--- frontend/components/nav/nav_container.js
+++ frontend/components/nav/nav_container.js
@@ -1,12 +1,19 @@
 import { connect } from 'react-redux';
 import Nav from './nav';
+import { logout } from '../../actions/session_actions';
 
 const mapStateToProps = state => ({
   loggedIn: Boolean(state.session.currentUser),
   currentUser: state.session.currentUser
 });
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(logout())
+  };
+};
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(Nav);
